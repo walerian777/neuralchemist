a0193fe3a7e68801b4bad77d27a37ad294ddd1ce
Walerian <walerian.sobczak@getbase.com>
2017-12-20 23:32:23 +0100

    Added package info to mix.exs.

--- mix.exs
+++ mix.exs
@@ -5,9 +5,13 @@ defmodule Neuralchemist.Mixfile do
     [
       app: :neuralchemist,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Neuralchemist",
+      source_url: "https://github.com/walerian777/neuralchemist"
     ]
   end
 
@@ -20,6 +24,20 @@ defmodule Neuralchemist.Mixfile do
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
       {:distancia, "~> 0.1.2"},
       {:ex_doc, "~> 0.18", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Walerian Sobczak"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/walerian777/neuralchemist"}
     ]
   end
 end
