ee358d97c597f7152fcbb95d81d2fd801b6288b1
Walerian <walerian.sobczak@getbase.com>
2017-12-30 20:16:51 +0100

    Added moduledoc to the Neuralchemist.NearestNeighbours module.

--- lib/neuralchemist/nearest_neighbours.ex
+++ lib/neuralchemist/nearest_neighbours.ex
@@ -1,4 +1,8 @@
 defmodule Neuralchemist.NearestNeighbours do
+  @moduledoc """
+  A module which provides `predict` function based on k-nearest neighbours algorithm.
+  """
+
   @default_params %{
     k_neighbours: 5,
     weights: :uniform,
@@ -7,15 +11,16 @@ defmodule Neuralchemist.NearestNeighbours do
 
   def predict(training_data, test_data, params \\ %{}) do
     params = Map.merge(@default_params, params)
+    k_neighbours = Map.get(params, :k_neighbours)
     training_data
     |> map_to_distance(test_data, params)
     |> Enum.sort_by(&elem(&1, 1))
-    |> Enum.take(Map.get(params, :k_neighbours))
+    |> Enum.take(k_neighbours)
     |> take_mode()
     |> elem(0)
   end
 
-  defp map_to_distance(training_data, test_data, %{ distance: distance }) do
+  defp map_to_distance(training_data, test_data, %{distance: distance}) do
     Enum.map(training_data, fn(data) ->
       label = elem(data, 0)
       sample = elem(data, 1)
